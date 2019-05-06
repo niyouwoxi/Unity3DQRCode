b2ed202278253825da0b165292c7094c77d6a1a0
van <>
2019-05-06 20:37:05 +0800

    initial commit

---
+++ Assets/PPCreateQRCode.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+using ZXing;
+using ZXing.QrCode;
+
+public class PPCreateQRCode : MonoBehaviour {
+	private BarcodeWriter mWriter;
+
+	public RawImage QRimage;
+
+	private Texture2D encoded ;
+	void Start () {
+		encoded = new Texture2D(200,200,TextureFormat.RGBA32,false);
+		var colors  = Encode("替换为要生成的二维码内容",200,200);
+		encoded.SetPixels32(colors);
+		encoded.Apply();
+		QRimage.texture = encoded;
+	
+	}
+	private static Color32[] Encode(string textForEncoding, int width, int height)
+	{
+		var writer = new BarcodeWriter
+		{
+			Format = BarcodeFormat.QR_CODE,
+			Options = new QrCodeEncodingOptions
+			{
+				Height = height,
+				Width = width,
+				Margin = 2,
+				PureBarcode = true
+			}
+		};
+		return writer.Write(textForEncoding);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/PPScanQRCode.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ZXing;
+
+public class PPScanQRCode : MonoBehaviour {
+	public RawImage rawImage;
+	private WebCamTexture mWebCamTexture;
+	private BarcodeReader mReader;
+	// Use this for initialization
+	void Start () {
+		mReader = new BarcodeReader();
+		WebCamDevice[] devices = WebCamTexture.devices;
+		#if UNITY_EDITOR
+			  CreateWebcamTex(devices[0].name);
+		#else
+		foreach (var item in devices)
+		{
+			if(!item.isFrontFacing){
+				CreateWebcamTex(item.name);
+        break;
+			}	
+		}
+		  #endif  
+
+		StartCoroutine(Scan());
+	}
+	private void CreateWebcamTex(string deviceName)
+	{
+		mWebCamTexture = new WebCamTexture(deviceName,1280, 720);
+		rawImage.texture = mWebCamTexture;
+		mWebCamTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private IEnumerator Scan()
+	{
+		yield return new WaitForSeconds(0.2F);
+		yield return new WaitForEndOfFrame();
+		if (mWebCamTexture != null && mWebCamTexture.width > 100)
+		{
+			string result = Decode(mWebCamTexture.GetPixels32(), mWebCamTexture.width, mWebCamTexture.height);
+			if(!string.IsNullOrEmpty(result)){
+					mWebCamTexture.Stop();		
+			}else{
+				StartCoroutine(Scan());
+			}
+		}
+	}
+	public string Decode(Color32[] colors, int width, int height)
+	{
+			var result = mReader.Decode(colors, width, height);
+			if (result != null)
+			{
+				return result.Text;
+			}
+			return null;
+	}
+
+}
